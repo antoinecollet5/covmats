9f78eaefaa4f0cf72202d11529a7bacfa56a81d5
antoinecollet5 <antoine.collet5@gmail.com>
2026-08-08 16:23:07 +0200

    FIX: test covariances

--- tests/test_covariances.py
+++ tests/test_covariances.py
@@ -405,7 +405,8 @@ def test_CovViaSparseCholesky(is_embbed_covariance: bool) -> None:
     # Test solve
     expected_x = np.arange(N, dtype=np.float64)
     b = A @ expected_x
-    assert np.allclose(cov.solve(b), expected_x)
+    np.testing.assert_allclose(cov @ expected_x, b)
+    np.testing.assert_allclose(cov.solve(b), expected_x, atol=1e-10)
 
     # Test colorize and whiten
     np.testing.assert_allclose(
@@ -462,6 +463,7 @@ def test_CovViaPrecisionCholesky(is_embbed_precision: bool) -> None:
     # Test matvec and solve
     expected_x = np.arange(n, dtype=np.float64)
     b = A @ expected_x
+    np.testing.assert_allclose(cov @ expected_x, b)
     np.testing.assert_allclose(cov.solve(b), expected_x, atol=1e-10)
 
     # Test colorize and whiten
